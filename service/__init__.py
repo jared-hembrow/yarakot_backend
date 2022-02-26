a38bdac30a8480bf965a56067cf89877c7866b6f
Jared Adam Hembrow <jared.hembrow@gmail.com>
2022-02-26 18:00:32 +0200

    cors

--- service/__init__.py
+++ service/__init__.py
@@ -20,7 +20,7 @@ db = SQLAlchemy(app)
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 from service.routes import get_routes
