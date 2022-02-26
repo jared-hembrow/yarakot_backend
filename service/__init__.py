1059809cb028baa8f7e57e8cf01a0ba46e7db731
Jared Adam Hembrow <jared.hembrow@gmail.com>
2022-02-26 17:58:41 +0200

    cors

--- service/__init__.py
+++ service/__init__.py
@@ -16,6 +16,13 @@ app.secret_key = getenv("SECRET_STRING")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 from service.routes import get_routes
 from service.routes import create_tables
 from service.routes import login_route
